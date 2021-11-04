c9aa3760a60f76fb6c83c8b9f0736468a9c6c194
ceastld <61349137+dyliu02@users.noreply.github.com>
2021-11-04 17:35:20 +0800

    小键盘

---
+++ WpfApp1/Tools/Keyboard.cs
@@ -0,0 +1,28 @@
+﻿using Quicker.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WpfApp1.Tools
+{
+    internal class Keyboard
+    {
+        public bool GetKeyBoardList()
+        {
+            var keyDownList = new List<string>();
+            if (!KeyboardHelper.IsAnyKeyDown()) return false;
+            for (int i = 1; i < Enum.GetValues(typeof(Keys)).Length; i++)
+            {
+                Keys key = (Keys)((System.Collections.IList)Enum.GetValues(typeof(Keys)))[i];
+                if (KeyboardHelper.IsKeyDown((WindowsInput.Native.VirtualKeyCode)key))
+                {
+                    keyDownList.Add(key.ToString());
+                }
+            }
+            return true;
+        }
+    }
+}
