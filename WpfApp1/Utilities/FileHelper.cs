cc47fdb4996d5e0ac07e3938e25c82f6cd7e7005
ceastld <61349137+dyliu02@users.noreply.github.com>
2021-12-08 01:27:35 +0800

    add:文件数字重命名 add:load xamlclass

---
+++ WpfApp1/Utilities/FileHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Utilities
+{
+    internal class FileHelper : Test.ExpEnvironment
+    {
+        /// <summary>
+        /// 数字重命名文件，
+        /// 1.png,2.png,3.png,以此类推
+        /// </summary>
+        public static void ReNameFilesByNumber()
+        {
+            List<string> files = (List<string>)_context.GetVarValue("fileList");
+            int name_index = 0;
+            foreach (string x in files)
+            {
+                if (!File.Exists(x)) continue;
+                string dir = Path.GetDirectoryName(x);
+                string ext = Path.GetExtension(x);
+                while (true)
+                {
+                    var path_new = Path.Combine(dir, name_index + ext);
+                    name_index++;
+                    if (!File.Exists(path_new))
+                    {
+                        new FileInfo(x).MoveTo(path_new);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp1/Utilities/XamlClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+using System.Xml;
+
+namespace WpfApp1.Utilities
+{
+    internal class XamlClass
+    {
+        public void LoadXamlToAppxaml()
+        {
+            string resource = "";
+            var strR = new System.IO.StringReader(resource);
+            var xmlR = XmlReader.Create(strR);
+            var resourceDictionary = XamlReader.Load(xmlR) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+
+        }
+        public static T CreateXamlObject<T>(string str1) where T : class
+        {
+            StringReader str = new StringReader(str1);
+            XmlReader xmlReader = XmlReader.Create(str);
+            return XamlReader.Load(xmlReader) as T;
+        }
+    }
+}
